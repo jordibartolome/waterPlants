33b4c82e86110d57194cbb1306d60e66a85c8b38
Jordi Bartolomé <jordi.bartolome.guillen@gmail.com>
2015-03-28 19:39:34 -0400

    2 pumps

--- app/main.py
+++ app/main.py
@@ -1,61 +1,52 @@
 # coding=utf-8
-# import RPi.GPIO as GPIO
 import time
 import RPi.GPIO as GPIO ## Import GPIO library
 
-PIN_NUMBER = 7;
+FIRST_PUMP_PIN_NUMBER = 7;
+SECOND_PUMP_PIN_NUMBER = 11;
+
+FIRST_PUMP_SECONDS = 10;
+SECOND_PUMP_SECONDS = 15;
 
 class WaterPlants(object):
 
 	def __init__(self):
 		print ("Initializing...")
+		initialize();
 
-	def start(self):
-		print ("Start")
+	def initialize():
 		GPIO.setmode(GPIO.BOARD) ## Use board pin numbering
-		GPIO.setup(PIN_NUMBER, GPIO.OUT) ## Setup GPIO Pin 7 to OUT
-		GPIO.output(PIN_NUMBER, True) ## Turn on GPIO pin 7
-		print GPIO.input(PIN_NUMBER)
 
-	def stop(self):
-		print ("Stop")
-		GPIO.output(PIN_NUMBER, False) ## Turn on GPIO pin 7
-		GPIO.cleanup()
+		# First pump
+		GPIO.setup(FIRST_PUMP_PIN_NUMBER, GPIO.OUT)
+		GPIO.output(FIRST_PUMP_PIN_NUMBER, False)
+
+		# Second pump
+		GPIO.setup(SECOND_PUMP_PIN_NUMBER, GPIO.OUT)
+		GPIO.output(SECOND_PUMP_PIN_NUMBER, False)
+
+	def startPump(pinNumber):
+		GPIO.output(pinNumber, True)
+
+	def stopPump(pinNumber):
+		GPIO.output(pinNumber, False)
+
+	def doWateringProcess(pinNumber, seconds):
+		self.startPump(pinNumber)
+		time.sleep(seconds)
+		self.stopPump(pinNumber)
 
 	def waterPlants(self):
 		print("Watering plants...")
-		self.start()
-		time.sleep(10)
-		self.stop()
+		# First pump
+		doWateringProcess(FIRST_PUMP_PIN_NUMBER, FIRST_PUMP_SECONDS);
+
+		# Second pump
+		doWateringProcess(SECOND_PUMP_PIN_NUMBER, SECOND_PUMP_SECONDS);
+
+		# Clean up
+		GPIO.cleanup()
 		print("Done!")
 
 wp = WaterPlants()
 wp.waterPlants()
-
-
-
-# GPIO.setmode(GPIO.BCM)
-
-# GPIO.setup(23, GPIO.IN, pull_up_down = GPIO.PUD_DOWN)
-
-# GPIO.setup(24, GPIO.IN, pull_up_down = GPIO.PUD_UP)
-
-# while True:
-
-# 	GPIO.wait_for_edge(23, GPIO.RISING)
-
-# 	print(“Button 1 Pressed”)
-
-# 	GPIO.wait_for_edge(23, GPIO.FALLING)
-
-# 	print(“Button 1 Released”)
-
-# 	GPIO.wait_for_edge(24, GPIO.FALLING)
-
-# 	print(“Button 2 Pressed”)
-
-# 	GPIO.wait_for_edge(24, GPIO.RISING)
-
-# 	print(“Button 2 Released”)
-
-# GPIO.cleanup()
