1080e7d1bda90346146954ab16066a9c47dc4e6e
Jordi Bartolomé <jordi.bartolome.guillen@gmail.com>
2015-03-29 16:16:16 -0400

    2nd

--- app/main.py
+++ app/main.py
@@ -3,8 +3,8 @@ import time
 import RPi.GPIO as GPIO ## Import GPIO library
 
 SMALL_PLANTS_PUMP_PIN_NUMBER = 11;
-SMALL_PLANTS_TAKE_OUT_AIR_PUMP_PIN_NUMBER = 13;
-BIG_PLANTS_PUMP_PIN_NUMBER = 16;
+#SMALL_PLANTS_TAKE_OUT_AIR_PUMP_PIN_NUMBER = 13;
+BIG_PLANTS_PUMP_PIN_NUMBER = 13;
 
 TAKE_OUT_AIR_DELAY = 10;
 
@@ -13,26 +13,26 @@ BIG_PLANTS_PUMP_SECONDS = 15;
 
 class WaterPlants(object):
 
-	def __init__(self):
-		print ("Initializing...")
-		GPIO.setmode(GPIO.BOARD) ## Use board pin numbering
+    def __init__(self):
+        print ("Initializing...")
+        GPIO.setmode(GPIO.BOARD) ## Use board pin numbering
 
-		# Small plants pumps
-		GPIO.setup(SMALL_PLANTS_PUMP_PIN_NUMBER, GPIO.OUT)
-		GPIO.output(SMALL_PLANTS_PUMP_PIN_NUMBER, False)
-		
-		GPIO.setup(SMALL_PLANTS_TAKE_OUT_AIR_PUMP_PIN_NUMBER, GPIO.OUT)
-		GPIO.output(SMALL_PLANTS_TAKE_OUT_AIR_PUMP_PIN_NUMBER, False)
+        # Small plants pumps
+        GPIO.setup(SMALL_PLANTS_PUMP_PIN_NUMBER, GPIO.OUT)
+        GPIO.output(SMALL_PLANTS_PUMP_PIN_NUMBER, False)
+        
+        GPIO.setup(SMALL_PLANTS_TAKE_OUT_AIR_PUMP_PIN_NUMBER, GPIO.OUT)
+        GPIO.output(SMALL_PLANTS_TAKE_OUT_AIR_PUMP_PIN_NUMBER, False)
 
-		# Second pump
-		GPIO.setup(BIG_PLANTS_PUMP_PIN_NUMBER, GPIO.OUT)
-		GPIO.output(BIG_PLANTS_PUMP_PIN_NUMBER, False)
+        # Second pump
+        GPIO.setup(BIG_PLANTS_PUMP_PIN_NUMBER, GPIO.OUT)
+        GPIO.output(BIG_PLANTS_PUMP_PIN_NUMBER, False)
 
     def startPump(self, pinNumber):
         GPIO.output(pinNumber, True)
 
     def stopPump(self, pinNumber):
-		GPIO.output(pinNumber, False)
+        GPIO.output(pinNumber, False)
 
     def doWateringProcess(self, pinNumber, seconds):
         self.startPump(pinNumber)
@@ -42,13 +42,13 @@ class WaterPlants(object):
     def waterPlants(self):
         print("Watering plants...")
         # First pump
-		print ('Take out air start')
-		self.startPump(SMALL_PLANTS_TAKE_OUT_AIR_PUMP_PIN_NUMBER)
-		time.sleep(TAKE_OUT_AIR_DELAY);
+        #print ('Take out air start')
+        #self.startPump(SMALL_PLANTS_TAKE_OUT_AIR_PUMP_PIN_NUMBER)
+        #time.sleep(TAKE_OUT_AIR_DELAY);
         print ('Pump small plants')
         self.doWateringProcess(SMALL_PLANTS_PUMP_PIN_NUMBER, SMALL_PLANTS_PUMP_SECONDS);
-		print ('Take out air end')
-		self.stopPump(SMALL_PLANTS_TAKE_OUT_AIR_PUMP_PIN_NUMBER);
+        print ('Take out air end')
+        self.stopPump(SMALL_PLANTS_TAKE_OUT_AIR_PUMP_PIN_NUMBER);
 
 
         print ('pump2')
