c839bb2d2be57b1dc6a29898187eb22baa4b03fb
Jordi Bartolomé <jordi.bartolome.guillen@gmail.com>
2015-03-29 13:10:37 -0400

    take out air

--- app/main.py
+++ app/main.py
@@ -2,51 +2,62 @@
 import time
 import RPi.GPIO as GPIO ## Import GPIO library
 
-FIRST_PUMP_PIN_NUMBER = 7;
-SECOND_PUMP_PIN_NUMBER = 11;
+SMALL_PLANTS_PUMP_PIN_NUMBER = 11;
+SMALL_PLANTS_TAKE_OUT_AIR_PUMP_PIN_NUMBER = 13;
+BIG_PLANTS_PUMP_PIN_NUMBER = 16;
 
-FIRST_PUMP_SECONDS = 10;
-SECOND_PUMP_SECONDS = 15;
+TAKE_OUT_AIR_DELAY = 10;
+
+SMALL_PLANTS_PUMP_SECONDS = 10;
+BIG_PLANTS_PUMP_SECONDS = 15;
 
 class WaterPlants(object):
 
 	def __init__(self):
 		print ("Initializing...")
-		initialize();
-
-	def initialize():
 		GPIO.setmode(GPIO.BOARD) ## Use board pin numbering
 
-		# First pump
-		GPIO.setup(FIRST_PUMP_PIN_NUMBER, GPIO.OUT)
-		GPIO.output(FIRST_PUMP_PIN_NUMBER, False)
+		# Small plants pumps
+		GPIO.setup(SMALL_PLANTS_PUMP_PIN_NUMBER, GPIO.OUT)
+		GPIO.output(SMALL_PLANTS_PUMP_PIN_NUMBER, False)
+		
+		GPIO.setup(SMALL_PLANTS_TAKE_OUT_AIR_PUMP_PIN_NUMBER, GPIO.OUT)
+		GPIO.output(SMALL_PLANTS_TAKE_OUT_AIR_PUMP_PIN_NUMBER, False)
 
 		# Second pump
-		GPIO.setup(SECOND_PUMP_PIN_NUMBER, GPIO.OUT)
-		GPIO.output(SECOND_PUMP_PIN_NUMBER, False)
+		GPIO.setup(BIG_PLANTS_PUMP_PIN_NUMBER, GPIO.OUT)
+		GPIO.output(BIG_PLANTS_PUMP_PIN_NUMBER, False)
 
-	def startPump(pinNumber):
-		GPIO.output(pinNumber, True)
+    def startPump(self, pinNumber):
+        GPIO.output(pinNumber, True)
 
-	def stopPump(pinNumber):
+    def stopPump(self, pinNumber):
 		GPIO.output(pinNumber, False)
 
-	def doWateringProcess(pinNumber, seconds):
-		self.startPump(pinNumber)
-		time.sleep(seconds)
-		self.stopPump(pinNumber)
+    def doWateringProcess(self, pinNumber, seconds):
+        self.startPump(pinNumber)
+        time.sleep(seconds)
+        self.stopPump(pinNumber)
 
-	def waterPlants(self):
-		print("Watering plants...")
-		# First pump
-		doWateringProcess(FIRST_PUMP_PIN_NUMBER, FIRST_PUMP_SECONDS);
+    def waterPlants(self):
+        print("Watering plants...")
+        # First pump
+		print ('Take out air start')
+		self.startPump(SMALL_PLANTS_TAKE_OUT_AIR_PUMP_PIN_NUMBER)
+		time.sleep(TAKE_OUT_AIR_DELAY);
+        print ('Pump small plants')
+        self.doWateringProcess(SMALL_PLANTS_PUMP_PIN_NUMBER, SMALL_PLANTS_PUMP_SECONDS);
+		print ('Take out air end')
+		self.stopPump(SMALL_PLANTS_TAKE_OUT_AIR_PUMP_PIN_NUMBER);
 
-		# Second pump
-		doWateringProcess(SECOND_PUMP_PIN_NUMBER, SECOND_PUMP_SECONDS);
 
-		# Clean up
-		GPIO.cleanup()
-		print("Done!")
+        print ('pump2')
+        # Second pump
+        #self.doWateringProcess(BIG_PLANTS_PUMP_PIN_NUMBER, BIG_PLANTS_PUMP_SECONDS);
+
+        # Clean up
+        GPIO.cleanup()
+        print("Done!")
 
 wp = WaterPlants()
 wp.waterPlants()
